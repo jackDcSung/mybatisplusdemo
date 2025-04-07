e1a53d3e91fdd8c1e95d804b3afc6bf68ec7b68e
jack <likerockman102@hotmail.com>
2025-04-08 03:36:20 +0800

    測試BaseMapper的刪除功能

--- src/test/java/com/atguigu/MyBatisPlusTest.java
+++ src/test/java/com/atguigu/MyBatisPlusTest.java
@@ -8,7 +8,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @SpringBootTest
@@ -19,9 +22,8 @@ public class MyBatisPlusTest {
     private UserMapper userMapper;
 
 
-
     @Test
-    public void testSelectList(){
+    public void testSelectList() {
 
 
         // 1.通過條件構造器查詢一個list集合,若無條件
@@ -30,10 +32,40 @@ public class MyBatisPlusTest {
         list.forEach(System.out::println);
 
 
-
-
-
     }
+    @Test
+    public void testInsert(){
+
+        User user = new User();
+        user.setName("張三");
+        user.setAge(23);
+        user.setEmail("zhangsan@atguigu.com");
+        int result = userMapper.insert(user);
+        System.out.println("result "+result);
+        System.out.println("id "+user.getId());
+    }
+
+
+    @Test
+    public void testDelete(){
+//        userMapper.deleteById(1909324471597006850L);
+
+
+//        Map<String, Object> map = new HashMap<>();
+//        map.put("name", "張三");
+//        map.put("age", 23);
+//        int result = userMapper.deleteByMap(map);
+//        System.out.println("result " + result);
+        List<Long> list = Arrays.asList(1L, 2L, 3L, 4L);
+        userMapper.deleteBatchIds(list);
+        int result = userMapper.deleteBatchIds(list);
+
+        System.out.println("result " + result);
+    }
+
+
+
+
 
 
 
